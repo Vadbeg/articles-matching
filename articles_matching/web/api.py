a2b1c2b415bd5f6b11bd1e05836ec1c25f61fd1f
vadbeg <vadbeg@tut.by>
2021-11-01 17:17:09 +0300

    Add simple search template

--- articles_matching/web/api.py
+++ articles_matching/web/api.py
@@ -1,7 +1,8 @@
 """Module with API"""
 
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import HTMLResponse
+import fastapi
+from fastapi import FastAPI, Form, Request, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -15,6 +16,34 @@ app.mount(
 templates = Jinja2Templates(directory='articles_matching/web/templates')
 
 
-@app.get(path='/', response_class=HTMLResponse)
+@app.get(path='/', response_class=RedirectResponse)
+def root():
+    return RedirectResponse(
+        '/home',
+    )
+
+
+@app.get(path='/home', response_class=HTMLResponse)
 def main_page(request: Request) -> Response:
     return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.post(path='/home', response_class=RedirectResponse)
+def redirect_search_query(
+    query_text: str = Form(..., alias='query-text')
+) -> RedirectResponse:
+    url = (
+        app.url_path_for(
+            name='query',
+        )
+        + f'?query_text={query_text}'
+    )
+
+    return RedirectResponse(url=url, status_code=fastapi.status.HTTP_302_FOUND)
+
+
+@app.get('/query', response_class=HTMLResponse)
+def query(request: Request, query_text: str) -> Response:
+    return templates.TemplateResponse(
+        'search_result.html', {'request': request, 'query_text': query_text}
+    )
